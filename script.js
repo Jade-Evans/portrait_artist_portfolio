73488121a956ac62f3f2aae7ee40a475d9b377ef
Jade-Evans <jpgabb@msn.com>
2025-08-22 16:44:16 +0100

    feat(about & commissions page): add draft content to about page and fieldsets in commissions for quote generator

--- script.js
+++ script.js
@@ -5,25 +5,17 @@
 // Wait until the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
 
-//We start by assigning a variable to all image wrappers (seeing as we'll use a loop for a repeatable function)
+//Assign variable name to all image wrappers (for use in loop)
     const imageWrappers = document.querySelectorAll(".image-wrapper");
 
-//Here we are using a forEach to loop through every image-wrapper as assigned above
+//Use forEach function to loop through every image-wrapper as assigned above
 //we're assigning a function called "wrapper" which is declared within this to each iteration
     imageWrappers.forEach(function(wrapper){
-
-    //STEP ONE: For each wrapper we find, we first need to find the overlay element assigned to it
-    //we use the below for this, and assign it a variable name (const overlay)
-
         const overlay = wrapper.querySelector(".overlay");
-
-    //STEP TWO: we add an event listener to tell the program when we want the function to be executed:
-
         wrapper.addEventListener("mouseover", function(){
-        overlay.style.opacity = "0.7"
+            overlay.style.opacity = "0.7"
         })
-    //STEP THREE: we make sure when we're not hovered it is no longer dark by applying similar logic to above. 
-    wrapper.addEventListener("mouseout", function(){
+        wrapper.addEventListener("mouseout", function(){
         overlay.style.opacity = "0"
         })
     })
